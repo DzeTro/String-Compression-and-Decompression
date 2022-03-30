0736e0fc6271538a40113eab140a4c033f45dc11
Dzevad Trokic <d.trokic@cp-pro.de>
2022-03-30 14:39:21 +0200

    Projektdateien hinzufügen.

---
+++ String Compression and Decompression/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+string KompromierteDaten = "";
+int anzahl = 0;
+int index = 0;
+int i = 0;
+
+
+Console.WriteLine("Bitte die Buchstaben eingeben: ");
+string BildDaten = Console.ReadLine();
+
+while (index < BilDaten.GetLenght())
+{
+    anzahl = 0;
+    i = index;
+    while (i < BildDaten.GetLenght() && BildDaten[i] == BildDaten[index])
+    {
+        i = i++;
+        anzahl = anzahl++;
+    }
+}
+
+if (anzahl > 3)
+{
+    KompromierteDaten.Add("§");
+    KompromierteDaten.Add(anzahl);
+    KompromierteDaten.Add(BildDaten[index]);
+    index = index + anzahl;
+}
+else
+{
+    i = 0;
+    while (i < anzahl)
+    {
+        KompromierteDaten.Add(BildDaten[index]);
+        i = i++;
+    }
+    index = index + anzahl;
+}
